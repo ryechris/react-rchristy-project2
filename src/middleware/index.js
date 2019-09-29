4e714e1d79bd7232ab60e5a0aa8d2c5f1b36c14f
call900913 <call900913@icloud.com>
2019-09-29 10:46:30 -0400

    Fix import syntax mistake in middleware/index.js

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,5 +1,5 @@
 import logger from './logger'
 import thunk from 'redux-thunk'
-import applyMiddleware from 'redux'
+import { applyMiddleware } from 'redux'
 
 export default applyMiddleware(thunk, logger)

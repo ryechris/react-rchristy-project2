d442c69f5acf409044a606719ae1c41cdc9ad508
call900913 <call900913@icloud.com>
2019-09-29 10:45:33 -0400

    Fix spelling error in reducer file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
-import authedUser from './authedUaser'
+import { combineReducers } from 'redux'
+import authedUser from './authedUser'
 import users from './users'
 import questions from './questions'
-import { combineReducers } from 'redux'
 
 export default combineReducers({
   authedUser,

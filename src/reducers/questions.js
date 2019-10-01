104c9f650d916833592ce104371be0832d821aef
call900913 <call900913@icloud.com>
2019-10-01 13:12:27 -0400

    Complete questions reducer

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,4 +1,5 @@
-import { RECEIVE_QUESTIONS } from '../actions/questions'
+import { RECEIVE_QUESTIONS, ADD_QUESTION } from '../actions/questions'
+import { ADD_ANSWER } from '../actions/answers'
 
 export default function questions (state = {}, action) {
   switch(action.type) {
@@ -6,6 +7,25 @@ export default function questions (state = {}, action) {
       return {
         ...state,
         ...action.questions
+      }
+    case ADD_QUESTION :
+      console.log('The question is: ', action.question)
+      console.log('And ', action.question.optionOne)
+      return {
+        ...state,
+        [action.question.id]: action.question
+      }
+    case ADD_ANSWER :
+      const { answer, id, authedUser } = action
+      const question = state[id]
+      const votesKey = answer + 'Votes'
+
+      return {
+        ...state,
+        [action.id]: {
+          ...question,
+          [votesKey]: question[votesKey].concat([authedUser])
+        }
       }
     default :
       return state

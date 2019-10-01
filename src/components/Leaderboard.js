f07557d3a71bcfa7d51d7e4fa9e2e54cc4e54d1e
call900913 <call900913@icloud.com>
2019-10-01 05:00:31 -0400

    Add a Leaderboard component

---
+++ src/components/Leaderboard.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+function Leaderboard ({users}) {
+  return (
+    <ul>
+      {users.map((user) => (
+        <li className='user'>
+          <img src={user.avatarURL} alt={`${user.name}'s avatar`} />
+          <div>
+            <h1>{user.name}</h1>
+            <p>{user.questions}</p>
+            <p>{user.answers}</p>
+          </div>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+// { users: {} }
+function mapStateToProps ({users}) {
+  return {
+    users: Object.keys(users).map((id) => {
+      const { name, avatarURL, questions, answers } = users[id]
+      return {
+        id,
+        name,
+        avatarURL,
+        questions: questions.length,
+        answers: answers.length
+      }
+    }).sort((a,b) => b.questions + b.answers > a.questions + a.answers)
+  }
+}
+
+export default connect(mapStateToProps)(Leaderboard)

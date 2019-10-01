e8a257e463114dc2eecb396c296a31d859cd62fd
call900913 <call900913@icloud.com>
2019-10-01 13:12:48 -0400

    Complete users reducer

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,4 +1,6 @@
 import { RECEIVE_USERS } from '../actions/users'
+import { ADD_QUESTION } from '../actions/questions'
+import { ADD_ANSWER } from '../actions/answers'
 
 export default function (state = {}, action) {
   switch(action.type) {
@@ -6,6 +8,24 @@ export default function (state = {}, action) {
       return {
         ...state,
         ...action.users
+      }
+    case ADD_QUESTION :
+      const { author, id } = action.question
+      return {
+        ...state,
+        [author]: {
+          ...state[author],
+          questions: state[author].questions.concat([id])
+        }
+      }
+    case ADD_ANSWER :
+      const user =  state[action.authedUser]
+      return {
+        ...state,
+        [action.authedUser]: {
+          ...user,
+          answers: user.answers.concat([action.id])
+        }
       }
     default :
       return state

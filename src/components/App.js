e43f9237c6c69feebfa81995d07eeeb861b9308e
call900913 <call900913@icloud.com>
2019-10-01 13:17:17 -0400

    Add App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,36 @@
-import React from 'react'
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
 import { handleInitialData } from  '../actions/shared'
 import { connect } from 'react-redux'
+import Nav from './Nav'
 import Dashboard from './Dashboard'
+import Leaderboard from './Leaderboard'
+import AddQuestion from './AddQuestion'
+import Question from './Question'
 import LoadingBar from 'react-redux-loading'
 
-class App extends React.Component {
+class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData())
   }
   render() {
     return (
-      <div>
-        <LoadingBar/>
-        {this.props.loading === true ? null : <Dashboard />}
-      </div>
+      <BrowserRouter>
+        <Fragment>
+          <LoadingBar />
+          <div className=''>
+            <Nav />
+            {this.props.loading === true
+              ? null
+              : <div>
+                  <Route path='/' exact component={Dashboard} />
+                  <Route path='/leaderboard' component={Leaderboard} />
+                  <Route path='/questions/:id' component={Question} />
+                  <Route path='/add' component={AddQuestion} />
+                </div>}
+          </div>
+        </Fragment>
+      </BrowserRouter>
     )
   }
 }

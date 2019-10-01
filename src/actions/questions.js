fd4f17a028261b595803881a96b933ff9de87e83
call900913 <call900913@icloud.com>
2019-10-01 13:17:57 -0400

    Add questions action creators

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,32 @@
+import { saveQuestion } from '../utils/_DATA.js'
+import { showLoading, hideLoading } from 'react-redux-loading'
+
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
+export const ADD_QUESTION = 'ADD_QUESTION'
 
 export function receiveQuestions(questions) {
   return {
     type: RECEIVE_QUESTIONS,
     questions
+  }
+}
+
+function addQuestion(question) {
+  return {
+    type: ADD_QUESTION,
+    question
+  }
+}
+
+export function handleAddQuestion(question) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState()
+    dispatch(showLoading())
+    console.log('THIS IS THE QUESTION: ', question)
+    return saveQuestion({
+      ...question,
+      author: authedUser
+    }).then((question) => dispatch(addQuestion(question)))
+      .then(() => dispatch(hideLoading()))
   }
 }

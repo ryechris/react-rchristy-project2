7a97e2c53c8d0595c4866c0f2cf470b3bc5aae99
call900913 <call900913@icloud.com>
2019-10-01 13:15:42 -0400

    Add a UI that displays a WYR question

---
+++ src/components/Question.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleAddAnswer } from '../actions/answers'
+
+const voteKeys = ['optionOneVotes', 'optionTwoVotes']
+
+const getPercentage = (count, total) => {
+  return total === 0 ? '-' : parseInt(count/total * 100, 10)
+}
+
+class Question extends Component {
+  handleAnswer = (answer) => {
+    const { question, authedUser } = this.props
+    this.answered = true
+
+    this.props.dispatch(handleAddAnswer({
+      authedUser,
+      answer,
+      id: question.id
+    }))
+  }
+
+  render() {
+    if (this.props.question === null) {
+      return <p>404: Question does not exist.</p>
+    }
+    const { question, vote, authorAvatar } = this.props;
+    const totalVotes = voteKeys
+      .reduce((total, key) => total + question[key].length, 0);
+
+    return (
+      <div>
+        <h1>Would You Rather</h1>
+
+        <div className=''>
+          By <img src={authorAvatar} alt="Author's avatar" />
+        </div>
+
+        <ul>
+          {['optionOneText', 'optionTwoText'].map((key) => {
+            const count = question[key.slice(0,9) + 'Votes'].length
+            return (
+              <li
+                key={key}
+                onClick={() => {
+                  if (vote === null && !this.answered) {
+                    this.handleAnswer(key.slice(0,9))
+                  }
+                }}
+                className={`${vote === key.slice(0,9) ? 'selected' : ''}`}
+              >{vote === null
+                  ? question[key]
+                  : <div className=''>
+                      <span>{question[key]}</span>
+                      <span>{getPercentage(count, totalVotes)}% ({count})</span>
+                    </div>}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps({ authedUser, questions, users }, { match }) {
+  const { id } = match.params
+  // next we grab the question with that id like so:
+  const question = questions[id]
+
+  if (!question) {
+    return {
+      question: null
+    }
+  }
+
+  const vote = voteKeys.reduce((vote, key) => {
+    if (vote !== null) {
+      return vote.slice(0,9)
+    }
+    return question[key].includes(authedUser) ? key : vote
+  }, null)
+
+  return {
+    question,
+    vote,
+    authedUser,
+    authorAvatar: users[question.author].avatarURL
+  }
+}
+
+export default connect(mapStateToProps)(Question)

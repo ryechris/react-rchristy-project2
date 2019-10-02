9770a4c5a81d260a503364b6df38f545adcb36a2
call900913 <call900913@icloud.com>
2019-10-02 13:36:25 -0400

    Update user id in actions/shared

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -4,7 +4,7 @@ import { receiveQuestions } from './questions'
 import { setAuthedUser } from './authedUser'
 import { showLoading, hideLoading } from 'react-redux-loading'
 
-const AUTHED_USER_ID = 'tylermcginnis'
+const AUTHED_USER_ID = ''
 
 export function handleInitialData() {
   return (dispatch) => {
